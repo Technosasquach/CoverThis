bcc3aab24582030b0341d18cb06a9c141575d890
Shahak Kuba <kubashahak@gmail.com>
2018-09-01 14:02:27 +1000

    Updated Scrapper

--- Scrapper/IdGenerator.py
+++ Scrapper/IdGenerator.py
@@ -5,6 +5,8 @@ csv = pd.read_csv("./book30-listing-test.csv")
 ids = csv['AMAZON ID'].values
 images = csv['IMAGE URL'].values
 title = csv['TITLE'].values
+aurthor = csv['AURTHOR'].values
+category = csv['CATEGORY'].values
 #print(ids)
 
 
@@ -12,6 +14,8 @@ new_id = []
 new_images = []
 new_title = []
 new_summaries = []
+new_aurthor = []
+new_category = []
 
 blockchain = 20
 
@@ -23,14 +27,16 @@ for i in range(len(ids)):
 		new_images.append(images[i])
 		new_title.append(title[i])
 		new_summaries.append(sum)
+		new_aurthor.append(aurthor[i])
+		new_category.append(category[i])
+
 	blockchain = blockchain - 1
 	if blockchain == 0:
 		break
 		#printing to verify that it works
 
-finalInformation= {"Title": new_title, "Images": new_images, "Summary": new_summaries}
-
-df = pd.DataFrame(finalInformation, columns = ['Title', 'Images', 'Summary'])
+finalInformation= {"Title": new_title, "Images": new_images, "Summary": new_summaries, "Aurthor": new_aurthor, "Category": new_category}
+df = pd.DataFrame(finalInformation, columns = ['Title', 'Images', 'Summary', 'Aurthor', 'Category'])
 
 #exporting data to csv file
 df.to_csv('data.csv')
