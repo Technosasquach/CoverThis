d773cdbd6a9095681820710c55ff690c7c5c4e7b
Unknown <sackboy129@gmail.com>
2018-09-01 15:52:11 +1000

    MADE IT FUCKING WORK yeet

--- Scrapper/IdGenerator.py
+++ Scrapper/IdGenerator.py
@@ -18,31 +18,45 @@ new_summaries = []
 new_aurthor = []
 new_category = []
 
+def doit(s, e):
 
-total = len(ids)
-max = 100
-if max > 0:
-	total = max
-counter = 0
+	total = len(ids)
+	start = s
+	end = e
 
-for i in range(len(ids)):
-	sum = Scraper2.get_summary(ids[i])
-	time.sleep(1)
-	if sum != "":
-		#print(sum)
-		new_id.append(ids[i])
-		new_images.append(images[i])
-		new_title.append(title[i])
-		new_summaries.append(sum)
-		new_aurthor.append(aurthor[i])
-		new_category.append(category[i])
+	total = end - start
+	counter = 0
 
-	max = max - 1
-	if max == 0:
-		break
-		#printing to verify that it works
-	counter = counter + 1
-	print("--- WORKING --- (" + str(counter)+"/"+ str(total) + ")")
+	for i in range(start, end):
+		sum = Scraper2.get_summary(ids[i])
+		time.sleep(1)
+		if sum != "":
+			#print(sum)
+			new_id.append(ids[i])
+			new_images.append(images[i])
+			new_title.append(title[i])
+			new_summaries.append(sum)
+			new_aurthor.append(aurthor[i])
+			new_category.append(category[i])
+			#printing to verify that it works
+
+		counter = counter + 1
+		print("--- WORKING --- (" + str(counter)+"/"+ str(total) + ")")
+
+
+
+length = 100
+runningTotal = 0
+while runningTotal < length:
+	stat = runningTotal
+	for i in range(20):
+		runningTotal = runningTotal + 1
+	en = runningTotal
+	doit(stat, en)
+	time.sleep(5)
+
+
+
 
 finalInformation= {"Title": new_title, "Images": new_images, "Summary": new_summaries, "Aurthor": new_aurthor, "Category": new_category, "ID": new_id}
 df = pd.DataFrame(finalInformation, columns = ['Title', 'Images', 'Summary', 'Aurthor', 'Category', 'ID'])
