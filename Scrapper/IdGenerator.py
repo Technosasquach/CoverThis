2be4c112de5f22402f23c42c5cf6b2eb6439712a
Unknown <sackboy129@gmail.com>
2018-09-01 14:05:16 +1000

    Made a working thing

--- Scrapper/IdGenerator.py
+++ Scrapper/IdGenerator.py
@@ -13,7 +13,12 @@ new_images = []
 new_title = []
 new_summaries = []
 
-blockchain = 20
+
+total = len(ids)
+max = -1
+if max > 0:
+	total = max
+counter = 0
 
 for i in range(len(ids)):
 	sum = Scraper2.get_summary(ids[i])
@@ -23,10 +28,12 @@ for i in range(len(ids)):
 		new_images.append(images[i])
 		new_title.append(title[i])
 		new_summaries.append(sum)
-	blockchain = blockchain - 1
-	if blockchain == 0:
+	max = max - 1
+	if max == 0:
 		break
 		#printing to verify that it works
+	counter = counter + 1
+	print("--- WORKING --- (" + str(counter)+"/"+ str(total) + ")")
 
 finalInformation= {"Title": new_title, "Images": new_images, "Summary": new_summaries}
 
