58d0841d369138937a3789009d31e03a31c7ee32
Unknown <sackboy129@gmail.com>
2018-09-01 13:17:21 +1000

    Improved yeets

--- Scrapper/IdGenerator.py
+++ Scrapper/IdGenerator.py
@@ -17,11 +17,13 @@ blockchain = 10
 
 for i in range(len(ids)):
 	sum = Scraper2.get_summary(ids[i])
+	#print(sum)
 	if sum != "":
 		#print(sum)
 		new_id.append(ids[i])
 		new_images.append(images[i])
 		new_title.append(title[i])
+		#print(title[i])
 		new_summaries.append(sum)
 	blockchain = blockchain - 1
 	if blockchain == 0:
@@ -29,9 +31,8 @@ for i in range(len(ids)):
 		#printing to verify that it works
 
 
-for i in range(len(new_id)):
-	df = {"Title": new_title[i], "Images_Link": new_images[i], "Summary": new_summaries[i]}
-
-#exporting data to csv file	
+raw_data = {"Title": new_title, "Images_Link": new_images, "Summary": new_summaries}
+print(new_title)
+df = pd.DataFrame(raw_data, columns = ['Title', 'Images_Link', 'Summary'])
+#exporting data to csv file
 df.to_csv('data.csv', sep=',', encoding='utf-8')
-
--- Scrapper/Scraper2.py
+++ Scrapper/Scraper2.py
@@ -5,12 +5,15 @@ import re
 def get_summary(id):
     yoat = ""
     url = 'https://www.amazon.com.au/dp/' + id
-    page = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    page = requests.get(url, headers = headers)
     yeet = page.content.decode("utf-8")
     checker = re.findall(r"We're sorry. The Web address you entered is not a functioning page on our site", yeet, re.I|re.M)
 
     if(checker == []):
         matches = re.findall(r' <noscript[\s\S]*?\/div', yeet, re.I |re.M)
+
+
         if(matches != []):
 	        text = matches[0]
 	        text = text[20:]
